1923e4ac3301c558f92164a1e7fb305c5488696e
Jan Martin Langeland <jan.martin@metzum.no>
2025-08-13 15:24:44 +0200

    Update apt_install.sh

--- scripts/apt_install.sh
+++ scripts/apt_install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 #sudo add-apt-repository ppa:webupd8team/terminix -y
+curl -sSL -O https://packages.microsoft.com/config/ubuntu/25.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+
 sudo add-apt-repository ppa:hluk/copyq -y
 curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb https://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
@@ -12,6 +17,7 @@ sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /e
 
 wget -q -0 https://downloads.vivaldi.com/stable/vivaldi-stable_7.5.3735.62-1_amd64.deb
 sudo apt-get -y install ./vivaldi*.deb
+rm ./vivaldi-stable_7.5.3735.62-1_amd64.deb
 
 #curl -s https://s3.eu-central-1.amazonaws.com/jetbrains-ppa/0xA6E8698A.pub.asc | sudo apt-key add -
 #echo "deb http://jetbrains-ppa.s3-website.eu-central-1.amazonaws.com bionic main" | sudo tee /etc/apt/sources.list.d/jetbrains-ppa.list > /dev/null
@@ -26,12 +32,14 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1C61A2656FB57B7E4D
 #sudo add-apt-repository ppa:linuxuprising/java -y
 #curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 
-
-
 sudo apt-get update
 
 sudo apt-get install -y tilix \
 	copyq \
+ 	dotnet-sdk-8.0 \
+  	aspnetcore-runtime-9.0 \
+ 	dotnet-sdk-9.0 \
+  	aspnetcore-runtime-9.0 \
 	dropbox python3-gpg \
 	google-chrome-stable \
 	flameshot \
