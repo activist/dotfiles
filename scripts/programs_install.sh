fbfd995dc0754a740147bc94f51c393afc4d13ea
Jan Martin Langeland <jan.martin@metzum.no>
2025-08-13 15:17:00 +0200

    Update programs_install.sh

--- scripts/programs_install.sh
+++ scripts/programs_install.sh
@@ -8,4 +8,7 @@ LATEST_VERSION=$(curl --silent https://api.github.com/repos/tofuutils/tenv/relea
 curl -O -L "https://github.com/tofuutils/tenv/releases/latest/download/tenv_${LATEST_VERSION}_amd64.deb"
 sudo dpkg -i "tenv_${LATEST_VERSION}_amd64.deb"
 
+#nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
 echo "Done installing programs!"
