0bf53483d791d01a3118eb277e60f3c0b4942851
Jan Martin Langeland <jan.martin@metzum.no>
2025-08-13 15:07:47 +0200

    Update apt_install.sh

--- scripts/apt_install.sh
+++ scripts/apt_install.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:webupd8team/terminix -y
+#sudo add-apt-repository ppa:webupd8team/terminix -y
 sudo add-apt-repository ppa:hluk/copyq -y
 wget -O- https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
 sudo add-apt-repository "deb http://repository.spotify.com stable non-free" -y
 sudo add-apt-repository ppa:daniel.pavel/solaar -y
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams stable main" > /etc/apt/sources.list.d/teams.list'
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-curl -s https://s3.eu-central-1.amazonaws.com/jetbrains-ppa/0xA6E8698A.pub.asc | sudo apt-key add -
-echo "deb http://jetbrains-ppa.s3-website.eu-central-1.amazonaws.com bionic main" | sudo tee /etc/apt/sources.list.d/jetbrains-ppa.list > /dev/null
+#curl -s https://s3.eu-central-1.amazonaws.com/jetbrains-ppa/0xA6E8698A.pub.asc | sudo apt-key add -
+#echo "deb http://jetbrains-ppa.s3-website.eu-central-1.amazonaws.com bionic main" | sudo tee /etc/apt/sources.list.d/jetbrains-ppa.list > /dev/null
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo add-apt-repository ppa:mozillateam/thunderbird-stable -y
@@ -18,8 +17,9 @@ wget -O - https://repo.jotta.us/public.gpg | sudo apt-key add -
 echo "deb https://repo.jotta.us/debian debian main" | sudo tee /etc/apt/sources.list.d/jotta-cli.list
 echo "deb [arch=i386,amd64] http://linux.dropbox.com/ubuntu bionic main" | sudo tee /etc/apt/sources.list.d/dropbox.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
-sudo add-apt-repository ppa:linuxuprising/java -y
-curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+
+#sudo add-apt-repository ppa:linuxuprising/java -y
+#curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 
 
 
@@ -31,18 +31,17 @@ sudo apt-get install -y tilix \
 	google-chrome-stable \
 	flameshot \
 	firefox \
-	intellij-idea-ultimate \
 	jotta-cli \
 	maven \
 	meld \
-	nodejs \
-	openjdk-8-jdk openjdk-8-jre \
-	oracle-java11-set-default oracle-java11-installer \
-	shotwell \
 	solaar \
 	spotify-client \
 	sublime-text \
-	thunderbird \
-	teams
+	thunderbird
+
+#tenv
+LATEST_VERSION=$(curl --silent https://api.github.com/repos/tofuutils/tenv/releases/latest | jq -r .tag_name)
+curl -O -L "https://github.com/tofuutils/tenv/releases/latest/download/tenv_${LATEST_VERSION}_amd64.deb"
+sudo dpkg -i "tenv_${LATEST_VERSION}_amd64.deb"
 
 echo "Done installing apt packages!"
