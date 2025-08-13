75a51c7f95a83c75619ec418f4993541395030c1
Jan Martin Langeland <jan.martin@metzum.no>
2025-08-13 15:08:24 +0200

    Update programs_install.sh

--- scripts/programs_install.sh
+++ scripts/programs_install.sh
@@ -3,4 +3,9 @@
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
+#tenv
+LATEST_VERSION=$(curl --silent https://api.github.com/repos/tofuutils/tenv/releases/latest | jq -r .tag_name)
+curl -O -L "https://github.com/tofuutils/tenv/releases/latest/download/tenv_${LATEST_VERSION}_amd64.deb"
+sudo dpkg -i "tenv_${LATEST_VERSION}_amd64.deb"
+
 echo "Done installing programs!"
