a8cd884043003e38174a1bb1500075945631d582
Jan Martin Langeland <jan.martin@metzum.no>
2025-08-13 15:12:30 +0200

    Update apt_install.sh

--- scripts/apt_install.sh
+++ scripts/apt_install.sh
@@ -9,6 +9,10 @@ sudo add-apt-repository ppa:daniel.pavel/solaar -y
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+
+wget -q -0 https://downloads.vivaldi.com/stable/vivaldi-stable_7.5.3735.62-1_amd64.deb
+sudo apt-get -y install ./vivaldi*.deb
+
 #curl -s https://s3.eu-central-1.amazonaws.com/jetbrains-ppa/0xA6E8698A.pub.asc | sudo apt-key add -
 #echo "deb http://jetbrains-ppa.s3-website.eu-central-1.amazonaws.com bionic main" | sudo tee /etc/apt/sources.list.d/jetbrains-ppa.list > /dev/null
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
