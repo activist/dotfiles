0dbad34a91f85511006f09aa98c4450ab32ef60b
Jan Martin Langeland <jan.martin@metzum.no>
2025-08-14 14:42:38 +0200

    Update apt_install.sh

--- scripts/apt_install.sh
+++ scripts/apt_install.sh
@@ -51,7 +51,7 @@ sudo curl -s --compressed -o /etc/apt/sources.list.d/zettlr.list "https://apt.ze
 wget https://github.com/derailed/k9s/releases/latest/download/k9s_linux_amd64.deb && apt install ./k9s_linux_amd64.deb && rm k9s_linux_amd64.deb
 
 # Docker (ce)
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 #tenv
